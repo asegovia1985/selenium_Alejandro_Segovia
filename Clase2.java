93a3a594b055c2cff3427c32329b74e7561fc8b7
asegovia <alejandro.segovia27@gmail.com>
2021-06-13 20:03:11 -0300

    prueba subir primer archivo

---
+++ Clase2.java
@@ -0,0 +1,16 @@
+package Clase2;
+
+public class Clase2 {
+
+    //INICIO
+
+    public static void main(String args[]){
+        //mostrar holaMundo!
+        System.out.println("holaMundo!");
+
+    ///fin
+
+    }
+
+
+}
